57d5665930b1af03ec712a9aec6202d8d144a087
Kunal Kadam <kunalkadam2762001@gmail.com>
2025-06-18 10:43:02 +0530

    Update BASE_URL to production server another Url

--- src/main/java/com/conference/controller/MessageController.java
+++ src/main/java/com/conference/controller/MessageController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = {"http://localhost:3000" ,"https://video-conferencing-application-taupe.vercel.app"})
+@CrossOrigin(origins = {"http://localhost:3000" ,"https://video-conferencing-application-taupe.vercel.app","https://video-conferencing-application-mu.vercel.app/"})
 @RestController
 @RequestMapping("/messages")
 @RequiredArgsConstructor
